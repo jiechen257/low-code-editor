d651337d587397b01b26957db7b6a2af444ee5fc
JIE <1487503910@qq.com>
2022-05-18 14:57:00 +0800

    fix: 重置部分界面样式

--- src/window.js
+++ src/window.js
@@ -4,6 +4,14 @@
 
 import Vue from 'vue'
 
+// 样式初始化
+import '@/assets/css/reset.css'
+import '@/assets/css/minx.less'
+
+// 引入字体图标
+import '@/assets/iconfont/iconfont.css'
+import '@/assets/iconfont/iconfont.js'
+
 /* 引入elementUI */
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
